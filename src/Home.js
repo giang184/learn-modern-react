d3f9fc3fe2e233bf59eaf15eba6db4a24a7391bb
Andrew Giang <giang184@gmail.com>
2021-09-24 10:35:31 -0700

    add useEffect dependencies

--- src/Home.js
+++ src/Home.js
@@ -2,11 +2,14 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
+
   const [blogs, setBlogs] = useState([
     {title: 'website', body:'hello world', author: 'andrew', id:1},
     {title: 'party', body:'hello world', author: 'gloria', id:2},
     {title: 'top tips', body:'hello world', author: 'andrew', id:3},
   ]);
+
+  const [name, setName] = useState('andrew');
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id)
@@ -15,12 +18,17 @@ const Home = () => {
 
   useEffect(() => {
     console.log('use effect ran')
-  });
+    console.log(name)
+
+  }, [name]);
 
   return ( 
     <div className="home">
       <BlogList blogs={blogs} title = "Dummy Blogs" handleDelete={handleDelete}/>
-      <BlogList blogs={blogs.filter((blog) => blog.author === 'andrew')} title = "Andrew Blogs"/>
+      <p>{name}</p>
+      <button onClick={() =>setName('Phuc')}>Change Name</button>
+
+      <BlogList blogs={blogs.filter((blog) => blog.author === 'andrew')} title = "Andrew Blogs" handleDelete={handleDelete}/>
     </div>
   );
 }
