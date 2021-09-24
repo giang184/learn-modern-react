cc5f2803f9f579685a09deea2f22ed7777b6b11d
Andrew Giang <giang184@gmail.com>
2021-09-24 11:01:24 -0700

    add conditional loading message

--- src/Home.js
+++ src/Home.js
@@ -4,20 +4,24 @@ import BlogList from './BlogList';
 const Home = () => {
 
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then(res => {
-        return res.json()
-      }).then(data => {
-        console.log(data)
-        setBlogs(data)
-      });
+    setTimeout( () => {
+      fetch('http://localhost:8000/blogs')
+        .then(res => {
+          return res.json();
+        }).then(data => {
+          setBlogs(data);
+          setIsPending(false);
+        });
+    }, 500);
 
   }, []);
 
   return ( 
     <div className="home">
+      {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title = "Dummy Blogs" />}
 
       {blogs && <BlogList blogs={blogs.filter((blog) => blog.author === 'andrew')} title = "Andrew Blogs"/>}
