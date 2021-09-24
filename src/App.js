ec56f6095593bc55873f8d32dfc2e7d7410f91e9
Andrew Giang <giang184@gmail.com>
2021-09-24 09:08:10 -0700

    examples of dynamic values application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 
 function App() {
+  const title = "Welcome!"
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        {Math.random()*10}
       </div>
     </div>
   );
