b070c2e72ee7f55ab20fbfe9bf51b6863696b0bc
Andrew Giang <giang184@gmail.com>
2021-09-24 14:37:35 -0700

    add post request on form submission

--- src/Create.js
+++ src/Create.js
@@ -4,13 +4,21 @@ const Create = () => {
   const [title,setTitle] = useState('')
   const [body,setBody] = useState('')
   const [author,setAuthor] = useState('Gloria')
+  const [isPending, setIsPending] = useState(false)
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = {title, body, author};
+    setIsPending(true);
 
-    console.log(blog)
-
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(blog)
+    }).then(() => {
+      console.log("new blog added")
+      setIsPending(false)
+    })
   }
 
   return ( 
@@ -38,7 +46,8 @@ const Create = () => {
           <option value="Andrew">Andrew</option>
           <option value="Gloria">Gloria</option>
         </select>
-        <button>Add Blog</button>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Processing...</button>}
         <p>{title}</p>
         <p>{body}</p>
         <p>{author}</p>
